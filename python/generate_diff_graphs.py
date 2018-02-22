b025d68229c573d1bd51067f1a19160f8c6495ae
Arul Siva Murugan V <arulsmg@gmail.com>
2018-02-22 12:58:12 +0530

    Graph building

--- python/generate_diff_graphs.py
+++ python/generate_diff_graphs.py
@@ -6,9 +6,11 @@ import random
 import sys
 
 import argparse
+import difflib
 import git
 import json
 
+from py2neo import Graph, Node, Relationship, Path
 from python import function_extractor
 
 def get_details(repo, cmd_param , combined_functions):
@@ -19,6 +21,7 @@ def get_details(repo, cmd_param , combined_functions):
   status, output = commands.getstatusoutput(cmd)
   with open ("%(tmp_dir)s/%(prefix)d_%(run)d.out" % cmd_param) as f:
     call_graph = json.load(f)
+    f.close()
 
   for entry in call_graph:
     key = entry['referringFunction'] + "@" + entry['referringFile']
@@ -26,7 +29,6 @@ def get_details(repo, cmd_param , combined_functions):
       combined_functions[key] = {}
     method_known = combined_functions.get(key).get(cmd_param['run'], None)
     if not method_known:
-      print "referring", entry['referringFile'], "@", entry['referringFunction'],":", entry['referringLine']
       combined_functions.get(key)[cmd_param['run']] = function_extractor.get_method_content(cmd_param['directory'], entry['referringFile'],entry['referringFunction'], entry['referringLine'])
 
     key = entry['referredFunction'] + "@" + entry['referredFile']
@@ -34,8 +36,35 @@ def get_details(repo, cmd_param , combined_functions):
       combined_functions[key] = {}
     method_known = combined_functions.get(key).get(cmd_param['run'], None)
     if not method_known:
-      print "referring", entry['referringFile'], "@", entry['referringFunction'],":", entry['referringLine']
       combined_functions.get(key)[cmd_param['run']] = function_extractor.get_method_content(cmd_param['directory'], entry['referredFile'],entry['referredFunction'], entry['referredLine'])
+  return call_graph
+
+
+def build_graph(delta, call_graph1, call_graph2):
+  nodes = {}
+  graph = Graph(password='smd')
+  tx = graph.begin()
+  for key in delta.keys():
+    print key
+    nodes[key] = Node("function", title=key, content=delta[key])
+
+  for entry in call_graph1:
+    calling_fun = entry['referringFunction'] + "@" + entry['referringFile']
+    called_fun = entry['referredFunction'] + "@" + entry['referredFile']
+    relation = Relationship(nodes[calling_fun], "Original Call To", nodes[called_fun])
+    tx.create(relation)
+
+    
+  for entry in call_graph2:
+    calling_fun = entry['referringFunction'] + "@" + entry['referringFile']
+    called_fun = entry['referredFunction'] + "@" + entry['referredFile']
+    relation = Relationship(nodes[calling_fun], "Modified Call To", nodes[called_fun])
+    tx.create(relation)
+ 
+  tx.commit()
+
+    
+  
 
 
 def main():
@@ -63,41 +92,25 @@ def main():
               }
   cmd = "cd %(directory)s; java -jar %(differ_jar)s -f %(from)s -t %(to)s -o1 %(tmp_dir)s/%(prefix)d_1.txt -o2 %(tmp_dir)s/%(prefix)d_2.txt " % cmd_param
   status, output = commands.getstatusoutput(cmd)
-  """ 
-  repo.git.checkout(args.commit_from)
-  cmd = "%(opengrok_sh)s index %(directory)s/" % cmd_param
-  status, output = commands.getstatusoutput(cmd)
-  cmd = "java -jar %(scoper_jar)s -R %(environment)s -i %(tmp_dir)s/%(prefix)d_1.txt -o %(tmp_dir)s/%(prefix)d_1.out" % cmd_param
-  status, output = commands.getstatusoutput(cmd)
 
-  repo.git.checkout(args.commit_to)
-  cmd = "%(opengrok_sh)s index %(directory)s/" % cmd_param
-  status, output = commands.getstatusoutput(cmd)
-  cmd = "java -jar %(scoper_jar)s -R %(environment)s -i %(tmp_dir)s/%(prefix)d_2.txt -o %(tmp_dir)s/%(prefix)d_2.out" % cmd_param
-  status, output = commands.getstatusoutput(cmd)
-  with open ("%(tmp_dir)s/%(prefix)d_1.out" % cmd_param) as f:
-    call_graph1 = json.load(f)
-  with open ("%(tmp_dir)s/%(prefix)d_2.out" % cmd_param) as f:
-    call_graph2 = json.load(f)
-
-  combined_functions = {} 
-  repo.git.checkout(args.commit_from)
-  for entry in call_graph1:
-    key = entry['referringFunction'] + "@" + entry['referringFile']
-  """
   cmd_param['run'] = 1
   cmd_param['commit_id'] = args.commit_from
   combined_function = {}
-  get_details(repo, cmd_param, combined_function)
+  call_graph1 = get_details(repo, cmd_param, combined_function)
 
   cmd_param['run'] = 2
   cmd_param['commit_id'] = args.commit_to
-  get_details(repo, cmd_param, combined_function)
-   
-  print combined_function
+  call_graph2 = get_details(repo, cmd_param, combined_function)
+  delta_contents = {}
+  for key in combined_function.keys():
+    original = combined_function.get(key).get(1, [])
+    modified = combined_function.get(key).get(2, [])
+    delta_contents[key] = "".join(difflib.ndiff(original, modified))
+  # free up unused data structures
+  del combined_function
+  del repo
 
-
-
+  build_graph(delta_contents, call_graph1, call_graph2)
   
 
 if __name__ == "__main__":
